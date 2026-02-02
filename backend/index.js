676352dc783c7c5721fc76c21d4162e9a569ee1c
Ajaythakur000 <seemathakur252525@gmail.com>
2026-02-02 21:13:46 +0530

    1: Backend Foundation & DB Connection

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+
+dotenv.config({});
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    credentials: true
+}
+app.use(cors(corsOptions));
+
+const PORT = process.env.PORT || 3000;
+
+// Database connect hone ke baad hi server start hoga
+connectDB()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`⚙️ Server is running at port : ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log("MONGO db connection failed !!! ", err);
+    });
