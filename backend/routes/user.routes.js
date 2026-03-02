daa657d2e4e6f548f1e90835e28b932c569ec171
Ajaythakur000 <seemathakur252525@gmail.com>
2026-03-03 00:49:44 +0530

    created - multer,signup,login

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { register, login, logout, updateProfile } from "../controllers/user.controller.js";
-import isAuthenticated from "../middlewares/isAuthenticated.js"; 
+import isAuthenticated from "../middlewares/isAuthenticated.js";
+import {singleUpload} from "../middlewares/multer.js" ;
 const router = Router();
 
-router.route("/register").post(register);
+router.route("/register").post(singleUpload,register);
 router.route("/login").post(login);
 router.route("/logout").get(logout);
 
