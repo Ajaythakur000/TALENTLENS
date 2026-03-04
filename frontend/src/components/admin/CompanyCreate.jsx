7eec81ba0a6a7ea8c354840dddba67be12f6db77
Ajaythakur000 <seemathakur252525@gmail.com>
2026-03-04 23:14:37 +0530

    6.Design admin pages (companies.jsx,companytable.jsx)

---
+++ frontend/src/components/admin/CompanyCreate.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function CompanyCreate() {
+  return (
+    <div>
+        <Navbar/>
+        <div className= 'max-w 4xl mx-auto'>
+            
+        </div>
+    </div>
+  )
+}
+
+export default CompanyCreate
